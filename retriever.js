e07ce0281bc154cc78ed35a54f5a75cfbfa0032f
Phoenix Shepherd <jacobwitt27@gmail.com>
2020-07-24 13:50:21 -0500

    Update retriever.js

--- retriever.js
+++ retriever.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 const discord = require("discord.js");
+const config = require("./config.json");
 
 const Client = new discord.Client();
 const Settings = require("./config.json");
 
 // command related
 const regex = /[^\s]+/g;
+
+client.login(config.token);
