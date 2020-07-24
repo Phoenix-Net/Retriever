d125ea4c6775e6f930d80474788c7b6457d9a79b
Magik Manz <36787474+MagikManz@users.noreply.github.com>
2020-07-24 19:39:11 +0100

    Added settings to the bot
    yes

--- retriever.js
+++ retriever.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const discord = require("discord.js");
 
 const Client = new discord.Client();
+const Settings = require("./config.json");
 
 // command related
 const regex = /[^\s]+/g;
