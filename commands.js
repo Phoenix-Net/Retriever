2b1f205880b51cefebce6690d996a1cc8af9cc07
Phoenix Shepherd <jacobwitt27@gmail.com>
2020-07-27 15:41:01 -0500

    Remove / Move Assets, Swap links from custom CDN (Hornyfur) to Github for simplicity and trust.
    Discord Limitations at it again :(

--- commands.js
+++ commands.js
@@ -13,8 +13,8 @@ class commands {
             { name: 'What is Retriever?', value: 'Retriever is a Discord bot written purely for entertainment value, based on dog jokes.'},
             { name: 'What can it do?', value: 'Well, that depends on how you interpret how Retriever makes jokes!\nIf you enjoy dog jokes as much as we do, then we guarentee you\u0027ll enjoy him as much as we do!\n\nFor further information regarding commands, please see ``?help.``'},
          )
-         .setImage('https://cdn.hornyfur.com/rmCo.png') // Change of plans, square looks ugly.
-         .setFooter('The Retriever developer team, Phoenix#2432 and Magik Manz#9999', 'https://cdn.hornyfur.com/gjeQ.png') // Upload picture for footer ASAP. =)
+         .setImage('https://github.com/Phoenix-Net/Retriever-Assets/blob/master/assets/lineboi.png') // Change of plans, square looks ugly. OLD (https://cdn.hornyfur.com/rmCo.png)
+         .setFooter('The Retriever developer team, Phoenix#2432 and Magik Manz#9999', 'https://github.com/Phoenix-Net/Retriever-Assets/blob/master/assets/fetcherlogo.png') // Upload picture for footer ASAP. =) OLD (https://cdn.hornyfur.com/gjeQ.png)
          message.reply(aboutembed)
     }
 
@@ -26,7 +26,7 @@ class commands {
                 {name:'Commands:', value:'?about\n?help\n?ball\n?zoomies\n?bananaphone\n?brokendog\n?sowwy\n?what\n?pupachino', inline: true},
                 {name:'About Command:', value:'Displays information regarding Retriever.\nYou just ran that one silly!\nGo fetch!\nGotta go fast!\nBanana Phone!\nWe swear, you didn\u0027t break him.\nYou\u0027ve really upset him this time...\nWhat did you just say?\nMmm...', inline: true},
             )
-            .setFooter('Have any suggestions? Contact us via email! admin@gsheps.net', 'https://cdn.hornyfur.com/gjeQ.png')
+            .setFooter('Have any suggestions? Contact us via email! admin@gsheps.net', 'https://github.com/Phoenix-Net/Retriever-Assets/blob/master/assets/fetcherlogo.png')
             message.reply(helpembed)
     }
 
@@ -34,7 +34,7 @@ class commands {
        const zoomiesembed = new discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Gotta go fast! 🐕💨')
-        .setImage('https://cdn.hornyfur.com/6vDf.gif')
+        .setImage('https://github.com/Phoenix-Net/Retriever-Assets/blob/master/assets/zoomies.gif') // OLD (https://cdn.hornyfur.com/6vDf.gif)
         // For some reason or another, this image keeps unloading in the middle of the GIF outside of the network that the CDN is hosted on. Not sure why.
         message.reply(zoomiesembed)
     }
@@ -43,7 +43,7 @@ class commands {
         const bananaphoneembed = new discord.MessageEmbed()
             .setColor('#ffe135')
             .setTitle('_Bring ring ring ring ring!_  **Banana Phone!**')
-            .setImage('https://cdn.hornyfur.com/iEBD.gif') // Stupid discord limitations, can't put videos :(
+            .setImage('https://github.com/Phoenix-Net/Retriever-Assets/blob/master/assets/BananaPhone.gif') // Stupid discord limitations, can't put videos :( OLD (https://cdn.hornyfur.com/iEBD.gif)
             message.reply(bananaphoneembed)
     }
 
@@ -51,7 +51,7 @@ class commands {
         const brokendogembed = new discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Bro, did you see the dragons in the kitchen?')
-            .setImage('https://cdn.hornyfur.com/WtJQ.gif')
+            .setImage('https://github.com/Phoenix-Net/Retriever-Assets/blob/master/assets/brokendog.gif') // OLD (https://cdn.hornyfur.com/WtJQ.gif)
             message.reply(brokendogembed)
     }
 
@@ -59,7 +59,7 @@ class commands {
         const sowwyembed = new discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('I-I\u0027m sowwy, I didn\u0027t mean to...')
-            .setImage('https://cdn.hornyfur.com/GxBj.gif')
+            .setImage('https://github.com/Phoenix-Net/Retriever-Assets/blob/master/assets/sowwy.gif') // OLD (https://cdn.hornyfur.com/GxBj.gif)
             message.reply(sowwyembed)
     }
 
@@ -67,7 +67,7 @@ class commands {
         const dowhatembed = new discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Do what now?')
-            .setImage('https://cdn.hornyfur.com/5wtn.gif')
+            .setImage('https://github.com/Phoenix-Net/Retriever-Assets/blob/master/assets/what.gif')  // OLD (https://cdn.hornyfur.com/5wtn.gif)
             message.reply(dowhatembed)
     }
 
@@ -75,7 +75,7 @@ class commands {
         const pupachinoembed = new discord.MessageEmbed()
             .setColor('#036635')
             .setTitle('``*slurping noises*``')
-            .setImage('https://cdn.hornyfur.com/zuDb.gif')
+            .setImage('https://github.com/Phoenix-Net/Retriever-Assets/blob/master/assets/pupachino.gif')
             message.reply(pupachinoembed)
     }
 
